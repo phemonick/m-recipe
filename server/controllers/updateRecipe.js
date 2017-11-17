7cdf1a641e0b55a045f3a395ed51bc3b74cea7e3
phemy <phemy.smith@gmail.com>
2017-11-17 22:24:42 +0100

    restructured update recipe controller

--- server/controllers/updateRecipe.js
+++ server/controllers/updateRecipe.js
@@ -3,7 +3,26 @@ const data = db.recipes;
 
 class UpdateRecipe{
     static update(req, res){
-        res.status(200).send('update recipes')
+        const { id }= req.params;
+        
+        data.forEach((result) => {
+            if(result.id == id){
+                result.id = req.body.id;
+                result.name = req.body.name;
+                result.ingredients = req.body.ingredients;
+                result.direction = req.body.direction;
+                result.reviews = req.body.reviews;
+
+                res.status(200).send({
+                    message: 'edited',
+                    result: result
+                })
+            }
+            
+            })
+            res.status(404).send({
+                message: 'doesnt exist'
+        })
     }
 }
 export default UpdateRecipe;
