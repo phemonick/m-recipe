8aa6e1f7a4254dc91fc846fd69408d65796667ae
phemy <phemy.smith@gmail.com>
2017-11-17 21:02:55 +0100

    refactored code

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import Controller from '../controllers/apiController';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/recipes', Controller.postRecipes);
-router.post('/recipes', Controller.createRecipes);
+// router.get('/recipes', Controller.postRecipes);
+// router.post('/recipes', Controller.createRecipes);
+// router.post('/recipes/:id/reviews', Controller.postReview);
 
-export default router;
+// export default router;
+
+const routes = (router) => {
+    router.get('/recipes', Controller.postRecipes);
+    router.post('/recipes', Controller.createRecipes);
+    router.post('/recipes/:id/reviews', Controller.postReview);
+};
+
+export default routes;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import api from './routes/api'
 
 // Instance of the express app
 const app = express();
@@ -12,7 +13,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api',  (req, res) => {
+const router = express.Router();
+
+api(router);
+app.use('/api', api);
+
+app.get('/',  (req, res) => {
     res.status(200).send({
         message: 'Welcome to more recipe api',
     });
