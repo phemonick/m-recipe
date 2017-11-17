2449daa94409e3e09e1c741c788e264a35d6bb40
phemy <phemy.smith@gmail.com>
2017-11-17 20:59:45 +0100

    exported all callbacks

---
+++ server/controllers/index.js
@@ -0,0 +1,15 @@
+import CreateRecipe from './createRecipe'
+import DeleteRecipe from './deleteRecipe'
+import GetRecipe from './getRecipes'
+import MostVote from './mostVote'
+import PostReview from './postReview'
+import UpdateRecipe from './updateRecipe'
+
+export{
+    CreateRecipe,
+    DeleteRecipe,
+    GetRecipe,
+    MostVote,
+    PostReview,
+    UpdateRecipe
+}
