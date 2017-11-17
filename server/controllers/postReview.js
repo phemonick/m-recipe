39930891c12ddb6187437d359173b0e4a55d43b1
phemy <phemy.smith@gmail.com>
2017-11-17 21:01:06 +0100

    callback for reviews route

---
+++ server/controllers/postReview.js
@@ -0,0 +1,20 @@
+import db from '../data/db';
+const data = db.recipes;
+
+class PostReview {
+
+    static review(req, res){
+        const{ id }= req.params;
+        let recipe = {};
+        data.forEach((result) => {
+            if(result.id === id){
+                result.reviews = req.body.reviews;
+                recipe = result;
+            }
+        })
+        res.status(200).send(result)
+    
+    }
+    
+}
+export default PostReview;
